e0068213cfeee47308715e3b6ca22be4874ca372
cannotbenormal <113307480+cannotbenormal@users.noreply.github.com>
2022-10-08 07:14:20 +0700

    Update add-akun.sh

--- menu/add-akun.sh
+++ menu/add-akun.sh
@@ -308,7 +308,7 @@ END
 
 #
 #buatvmess
-vmessnomalak={"add":"141.193.213.20","aid":"0","host":"${domain}","id":"${uuid}","net":"ws","path":"wss:\/\/${domain}\/xrayvws","port":"443","ps":"xl vmess ws by ado xray mantap","tls":"tls","sni":"${domain}","type":"none","v":"2"}
+vmessnomalak={""add"":""141.193.213.20"",""aid"":""0"",""host"":""${domain}"",""id"":""${uuid}"",""net"":""ws"",""path"":""wss:\/\/${domain}\/xrayvws"",""port"":""443"","ps":""xl vmess ws by ado xray mantap"",""tls"":""tls"",""sni"":""${domain}"",""type"":""none"",""v"":""2""}
 vmessnomalakencode=$(echo -ne "$vmessnomalak" | base64);
 #
 clear
